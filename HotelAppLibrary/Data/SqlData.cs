567a931e2d994d9a50e22bcdde524596b6575b55
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-01 00:27:11 +0100

    HotelAppLibrary - SqlData

---
+++ HotelAppLibrary/Data/SqlData.cs
@@ -0,0 +1,22 @@
+﻿using HotelAppLibrary.Database;
+using HotelAppLibrary.Models;
+
+namespace HotelAppLibrary.Data
+{
+	public class SqlData
+	{
+		private readonly ISqlDataAccess _db;
+		private const string _connectionStringName = "SqlDb";
+
+		public SqlData(ISqlDataAccess db)
+		{
+			_db = db;
+		}
+
+		public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+		{
+			return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+				new { startDate, endDate }, _connectionStringName, true);
+		}
+	}
+}
