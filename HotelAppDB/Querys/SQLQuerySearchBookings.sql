095ccf1519427c89b020f1e172d2038928895360
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-02 00:47:06 +0100

    spBookings_Search

---
+++ HotelAppDB/Querys/SQLQuerySearchBookings.sql
@@ -0,0 +1,11 @@
+﻿
+
+	select [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate],
+		[b].[CheckedIn], [b].[TotalCost], [g].[FirstName], [g].[LastName],
+		[r].[RoomNumber], [r].[RoomTypeId], [rt].[Title], [rt].[Description], [rt].[Price]
+	from dbo.Bookings b
+	inner join dbo.Guests g on b.GuestId = g.Id
+	inner join dbo.Rooms r on b.RoomId = r.Id
+	inner join dbo.RoomTypes rt on r.RoomTypeId = rt.Id
+	where b.StartDate = '2023-12-5';
+
---
+++ HotelAppDB/dbo/Stored Procedures/spBookings_Search.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Search]
+	@lastName nvarchar(50),
+	@startDate date
+AS
+begin
+	set nocount on;
+
+	select [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate],
+		[b].[CheckedIn], [b].[TotalCost], [g].[FirstName], [g].[LastName],
+		[r].[RoomNumber], [r].[RoomTypeId], [rt].[Title], [rt].[Description], [rt].[Price]
+	from dbo.Bookings b
+	inner join dbo.Guests g on b.GuestId = g.Id
+	inner join dbo.Rooms r on b.RoomId = r.Id
+	inner join dbo.RoomTypes rt on r.RoomTypeId = rt.Id
+	where b.StartDate = @startDate and g.LastName = @lastName;
+end
