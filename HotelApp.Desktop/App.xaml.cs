7212ea4c20aabbd577e7f7fa11b151f9a698b238
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-04 12:58:59 +0100

    HotelApp.Desktop

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using HotelAppLibrary.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelApp.Desktop
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var services = new ServiceCollection();
+			services.AddTransient<MainWindow>();
+			services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			IConfiguration config = builder.Build();
+
+			services.AddSingleton(config);
+
+			var serviceProvider = services.BuildServiceProvider();
+			var mainWindow = serviceProvider.GetService<MainWindow>();
+
+			mainWindow?.Show();
+		}
+	}
+}
